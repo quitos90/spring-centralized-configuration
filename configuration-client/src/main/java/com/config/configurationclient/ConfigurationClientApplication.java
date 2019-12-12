76f3156c5aa82019215073ff64ecc0bd6190d94e
Marcos <martinalvarez.marcos@gmail.com>
2019-12-12 11:34:51 +0100

    first commit

---
+++ configuration-client/src/main/java/com/config/configurationclient/ConfigurationClientApplication.java
@@ -0,0 +1,13 @@
+package com.config.configurationclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationClientApplication.class, args);
+	}
+
+}
---
+++ configuration-client/src/test/java/com/config/configurationclient/ConfigurationClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.config.configurationclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
