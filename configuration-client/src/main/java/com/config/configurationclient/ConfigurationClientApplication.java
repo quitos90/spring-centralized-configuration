47b7b89e4b58e16bcc293832b2576a0ea2955ad1
Marcos <martinalvarez.marcos@gmail.com>
2019-12-12 11:50:58 +0100

    config server working

--- configuration-client/src/main/java/com/config/configurationclient/ConfigurationClientApplication.java
+++ configuration-client/src/main/java/com/config/configurationclient/ConfigurationClientApplication.java
@@ -1,7 +1,11 @@
 package com.config.configurationclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ConfigurationClientApplication {
@@ -10,4 +14,16 @@ public class ConfigurationClientApplication {
 		SpringApplication.run(ConfigurationClientApplication.class, args);
 	}
 
+	@RefreshScope
+	@RestController
+	class MessageRestController {
+
+		@Value("${message:Hello default}")
+		private String message;
+
+		@RequestMapping("/message")
+		String getMessage() {
+			return this.message;
+		}
+	}
 }
